2661f2d551147309bc1a9440261fd47bddb5ff1f
tanguy <tanguy@DESKTOP-S882JLB.localdomain>
2021-05-04 04:43:24 -0700

    adding pig

---
+++ Assets/player_enemy/Enemy/pig/pigWeaponScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pigWeaponScript : MonoBehaviour
+{
+    private float timer = 0;
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    public GameObject bullet1;
+
+    bool tempo = false;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D (Collider2D Info)
+    {
+        if (Info.tag == "enemy")
+            Physics2D.IgnoreCollision(Info.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+        else
+            Destroy(gameObject);
+        if (Info.tag == "Player")
+            Info.GetComponent<player_controler>().takeDamage();
+    }
+
+}
+
+
---
+++ Assets/player_enemy/Enemy/pig/weapon_pig.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon_pig : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject pig;
+
+    private bool single_bullet;
+    public GameObject bulletPrefab;
+
+    private float timer;
+    bool tempo = false;
+
+    void Start()
+    {
+        single_bullet = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (pig.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Hit") && single_bullet ==false)
+        {
+            timer += Time.deltaTime;
+            if (timer > 0.3)
+            {
+              Instantiate(bulletPrefab,firePoint.position, firePoint.rotation);
+              single_bullet = true;
+            }
+        }
+        if (pig.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Pig") && single_bullet == true)
+            single_bullet = false;
+    }
+}
